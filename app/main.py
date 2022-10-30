8b4b767d63fb1e32a988ff274ae070d07c69f253
jmetzg11alpine <jmetzg11_alpine@mailfence.com>
2022-10-29 21:00:58 -0700

    added cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI 
+from fastapi.middleware.cors import CORSMiddleware
 from supabase import create_client, Client 
 from .config import settings 
 import time
@@ -6,6 +7,15 @@ from . import schemas
 from . import functions
 
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 while True:
     try:
